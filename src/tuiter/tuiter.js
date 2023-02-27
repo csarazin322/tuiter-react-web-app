0681896ddfb2c080997116ca727a8217eac47b1a
Corey Sarazin <coreysarazin@gmail.com>
2023-02-26 21:30:19 -0500

    add spacex

--- src/tuiter/tuiter.js
+++ src/tuiter/tuiter.js
@@ -1,9 +1,25 @@
 import Nav from "../nav";
+import NavigationSidebar from "./navigation-sidebar";
+import WhoToFollowListItem from "./who-to-follow/who-to-follow-list-item";
 
 function Tuiter() {
     return (
         <div>
             <Nav/>
+            <NavigationSidebar active='home'/>
+            <WhoToFollowListItem who={{
+                userName: 'NASA', handle: 'NASA',
+                avatarIcon: 'nasa.webp',
+            }}/>
+            <WhoToFollowListItem who={{
+                userName: 'Tesla', handle: 'tesla',
+                avatarIcon: 'tesla.jpg',
+            }}/>
+            <WhoToFollowListItem who={{
+                userName: 'SpaceX', handle: 'SpaceX',
+                avatarIcon: 'spacex.jpg',
+            }}/>
+
             <h1>Tuiter</h1>
         </div>
     );
