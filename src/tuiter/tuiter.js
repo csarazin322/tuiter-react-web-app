a9a7dd99b2f8f334f7314ba60aeb25e6aa7ff2d7
Corey Sarazin <coreysarazin@gmail.com>
2023-02-26 21:56:07 -0500

    finished hw

--- src/tuiter/tuiter.js
+++ src/tuiter/tuiter.js
@@ -1,26 +1,24 @@
 import Nav from "../nav";
 import NavigationSidebar from "./navigation-sidebar";
-import WhoToFollowListItem from "./who-to-follow/who-to-follow-list-item";
+import ExploreComponent from "./explore";
+import WhoToFollowList from "./who-to-follow/who-to-follow";
 
 function Tuiter() {
     return (
         <div>
             <Nav/>
-            <NavigationSidebar active='home'/>
-            <WhoToFollowListItem who={{
-                userName: 'NASA', handle: 'NASA',
-                avatarIcon: 'nasa.webp',
-            }}/>
-            <WhoToFollowListItem who={{
-                userName: 'Tesla', handle: 'tesla',
-                avatarIcon: 'tesla.jpg',
-            }}/>
-            <WhoToFollowListItem who={{
-                userName: 'SpaceX', handle: 'SpaceX',
-                avatarIcon: 'spacex.jpg',
-            }}/>
-
-            <h1>Tuiter</h1>
+            <div className="row mt-2">
+                <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                    <NavigationSidebar active="explore"/>
+                </div>
+                <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+                     style={{"position": "relative"}}>
+                    <ExploreComponent/>
+                </div>
+                <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                    <WhoToFollowList/>
+                </div>
+            </div>
         </div>
     );
 }
