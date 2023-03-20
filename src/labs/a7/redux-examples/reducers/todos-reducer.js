f62b6be839a647addaf59b23ac186f5e35176221
Corey Sarazin <coreysarazin@gmail.com>
2023-03-20 16:43:05 -0400

    lab done

--- src/labs/a7/redux-examples/reducers/todos-reducer.js
+++ src/labs/a7/redux-examples/reducers/todos-reducer.js
@@ -4,7 +4,7 @@ const initialTodos = [
     {
         _id: "123",
         do: "Accelerate the world's transition to sustainable energy",
-        done: false
+        done: true
     },
     {
         _id: "234",
@@ -16,6 +16,26 @@ const initialTodos = [
 const todosSlice = createSlice({
     name: 'todos',
     initialState: initialTodos,
+    reducers: {
+        addTodo(state, action) {
+            state.push({
+                _id: (new Date()).getTime(),
+                do: action.payload.do,
+                done: false
+            });
+        },
+        deleteTodo(state, action) {
+            const index = action.payload
+            state.splice(index, 1)
+        },
+        todoDoneToggle(state, action) {
+            const todo = state.find((todo) =>
+                todo._id === action.payload._id)
+            todo.done = !todo.done
+        }
+    }
+
 });
 
+export const { addTodo, deleteTodo, todoDoneToggle } = todosSlice.actions
 export default todosSlice.reducer
