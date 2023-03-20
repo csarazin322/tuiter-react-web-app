cd9edf38168fc2c5aaaa26fcdf5ef5b9a2e6bb3b
Corey Sarazin <coreysarazin@gmail.com>
2023-03-20 12:42:59 -0400

    half way through the lab

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import './App.css';
 import Labs from "./labs";
 import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter/tuiter";
-import {BrowserRouter} from "react-router-dom";
-import {Route, Routes} from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="container">
                 <Routes>
-                    <Route index
-                           element={<Labs/>}/>
+                    <Route path='/*'
+                        element={<Labs />} />
                     <Route path="/hello"
-                           element={<HelloWorld/>}/>
+                        element={<HelloWorld />} />
                     <Route path="/tuiter"
-                           element={<Tuiter/>}/>
+                        element={<Tuiter />} />
                 </Routes>
             </div>
         </BrowserRouter>
